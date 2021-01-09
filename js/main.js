cac3eaebe6cdde312b1e84dba760fac80187a14a
pankaj26435 <parmarpankaj03@gmail.com>
2021-01-09 15:57:15 +0530

    Roque Sells

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+	$('#js-main-slider').pogoSlider({
+		autoplay: true,
+		autoplayTimeout: 4000,
+		displayProgess: true,
+		preserveTargetSize: true,
+		targetWidth: 1000,
+		targetHeight: 350,
+		responsive: true,				
+	}).data('plugin_pogoSlider');
+
+	var transitionDemoOpts = {
+		displayProgess: false,
+		generateNav: false,
+		generateButtons: false
+	}
+
+
+});
